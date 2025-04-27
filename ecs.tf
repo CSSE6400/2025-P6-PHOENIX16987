bba86ad3af01d14003ef80ac7e6e8bd30a8c01be
PHOENIX16987 <64996640+PHOENIX16987@users.noreply.github.com>
2025-04-27 12:23:11 +0000

    p6 summit

--- ecs.tf
+++ ecs.tf
@@ -50,13 +50,18 @@ resource "aws_ecs_service" "taskoverflow" {
   task_definition = aws_ecs_task_definition.taskoverflow.arn
   desired_count   = 1
   launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets             = data.aws_subnets.private.ids
-    security_groups     = [aws_security_group.taskoverflow.id]
-    assign_public_ip    = true
+  
+  load_balancer {
+    target_group_arn = aws_lb_target_group.taskoverflow.arn
+    container_name   = "taskoverflow"
+    container_port   = 6400
   }
 
+  network_configuration {
+    subnets          = data.aws_subnets.private.ids
+    security_groups  = [aws_security_group.taskoverflow.id]
+    assign_public_ip = true
+  }
 }
 
 resource "aws_security_group" "taskoverflow" {
